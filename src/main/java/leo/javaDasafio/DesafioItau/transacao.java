decc6044e6035e676beb2ca4a40ca56ffe374c2d
Leonardo250 <leomoulin2002@gmail.com>
2026-07-09 21:10:37 -0300

    Assinaturas colocadas em transacao

--- src/main/java/leo/javaDasafio/DesafioItau/transacao.java
+++ src/main/java/leo/javaDasafio/DesafioItau/transacao.java
@@ -1,8 +1,15 @@
 package leo.javaDasafio.DesafioItau;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class transacao  {
 
     private BigDecimal valor;
