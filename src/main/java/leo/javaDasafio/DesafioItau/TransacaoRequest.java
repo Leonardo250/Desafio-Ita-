ce6c8bada91a44e9ffa1ff88f96d86d6d5862c96
Leonardo250 <leomoulin2002@gmail.com>
2026-07-22 23:44:43 -0300

    DTO para transações criado

---
+++ src/main/java/leo/javaDasafio/DesafioItau/TransacaoRequest.java
@@ -0,0 +1,11 @@
+package leo.javaDasafio.DesafioItau;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+public class TransacaoRequest {
+    private BigDecimal valor;
+    private OffsetDateTime dataHora;
+
+
+}
--- src/main/java/leo/javaDasafio/DesafioItau/TransacoesController.java
+++ src/main/java/leo/javaDasafio/DesafioItau/TransacoesController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransacoesController {
 
     @PostMapping
-    public ResponseEntity adicionar() {
+    public ResponseEntity adicionar(@RequestBody TransacaoRequest transacaoRequest) {
         return  ResponseEntity.status(HttpStatus.CREATED).build();
 
 
